bf1c3c1814edc51d593ebdd65da63f25d3b749aa
Konstantinos Spyrou <spyroukon@gmail.com>
2021-11-22 16:22:53 +0200

    init

---
+++ src/main/java/eu/eosc/observatory/ObservatoryApplication.java
@@ -0,0 +1,13 @@
+package eu.eosc.observatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservatoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservatoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/eosc/observatory/ObservatoryApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.eosc.observatory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservatoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
