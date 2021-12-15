49e0da7837a581b77005d6f74d075612ef07e5a5
Konstantinos Spyrou <spyroukon@gmail.com>
2021-12-15 14:19:38 +0200

    created equals and hashCode

--- src/main/java/eu/eosc/observatory/domain/Stakeholder.java
+++ src/main/java/eu/eosc/observatory/domain/Stakeholder.java
@@ -4,6 +4,7 @@ import eu.eosc.observatory.service.Identifiable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Stakeholder implements Identifiable<String> {
 
@@ -109,5 +110,18 @@ public class Stakeholder implements Identifiable<String> {
                     .findFirst()
                     .orElseThrow(() -> new IllegalArgumentException("unknown value: " + s));
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Stakeholder)) return false;
+        Stakeholder that = (Stakeholder) o;
+        return id.equals(that.id) && Objects.equals(name, that.name) && Objects.equals(type, that.type) && Objects.equals(subType, that.subType) && Objects.equals(country, that.country) && Objects.equals(associationMember, that.associationMember) && Objects.equals(managers, that.managers) && Objects.equals(contributors, that.contributors);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type, subType, country, associationMember, managers, contributors);
     }
 }
