8c94750e2df8ff72143b37cf51d0694ed0d15b28
Konstantinos Spyrou <spyroukon@gmail.com>
2023-10-12 20:00:04 +0300

    fixed exception thrown in security expressions when the user is not found in the db

--- src/main/java/eu/eosc/observatory/service/SecurityService.java
+++ src/main/java/eu/eosc/observatory/service/SecurityService.java
@@ -2,9 +2,10 @@ package eu.eosc.observatory.service;
 
 import eu.eosc.observatory.domain.User;
 import eu.eosc.observatory.permissions.PermissionService;
+import gr.athenarc.catalogue.exception.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -21,26 +22,26 @@ public class SecurityService {
     }
 
     public boolean canRead(Authentication authentication, String resourceId) {
-        User user = userService.get(User.getId(authentication));
+        User user = getUserOrNull(authentication);
         // TODO: modify "!user.getPoliciesAccepted().isEmpty()" below according to specifications
-        return hasAcceptedPolicy(user) && permissionService.canRead(user.getId(), resourceId);
+        return user != null && hasAcceptedPolicy(user) && permissionService.canRead(user.getId(), resourceId);
     }
 
     public boolean canWrite(Authentication authentication, String resourceId) {
-        User user = userService.get(User.getId(authentication));
+        User user = getUserOrNull(authentication);
         // TODO: modify "!user.getPoliciesAccepted().isEmpty()" below according to specifications
-        return hasAcceptedPolicy(user) && permissionService.canRead(user.getId(), resourceId);
+        return user != null && hasAcceptedPolicy(user) && permissionService.canRead(user.getId(), resourceId);
     }
 
     public boolean canManage(Authentication authentication, String resourceId) {
-        User user = userService.get(User.getId(authentication));
+        User user = getUserOrNull(authentication);
         // TODO: modify "!user.getPoliciesAccepted().isEmpty()" below according to specifications
-        return hasAcceptedPolicy(user) && permissionService.canManage(user.getId(), resourceId);
+        return user != null && hasAcceptedPolicy(user) && permissionService.canManage(user.getId(), resourceId);
     }
 
     public boolean hasPermission(Authentication authentication, String action, String resourceId) {
-        User user = userService.get(User.getId(authentication));
-        return hasAcceptedPolicy(user) && permissionService.hasPermission(user.getId(), action, resourceId);
+        User user = getUserOrNull(authentication);
+        return user != null && hasAcceptedPolicy(user) && permissionService.hasPermission(user.getId(), action, resourceId);
     }
 
     public boolean hasAcceptedPolicy(User user) {
@@ -49,5 +50,22 @@ public class SecurityService {
         // TODO: modify "!user.getPoliciesAccepted().isEmpty()" below according to specifications
 //        return user.getPoliciesAccepted() != null && !user.getPoliciesAccepted().isEmpty();
         return true;
+    }
+
+    /**
+     * Searches the db for a user based on the Authentication.
+     * @param authentication
+     * @return {@link User}
+     */
+    @Nullable
+    User getUserOrNull(Authentication authentication) {
+        User user = null;
+        try {
+            user = userService.get(User.getId(authentication));
+        } catch (ResourceNotFoundException ignore) {
+            // in case user is not found in db
+            // skip
+        }
+        return user;
     }
 }
