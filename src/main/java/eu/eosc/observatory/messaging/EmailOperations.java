a10b6b8186e09615080764f09503dbd2a21d3923
Konstantinos Spyrou <spyroukon@gmail.com>
2023-10-12 19:48:43 +0300

    added documentation and updated email templates

--- src/main/java/eu/eosc/observatory/messaging/EmailOperations.java
+++ src/main/java/eu/eosc/observatory/messaging/EmailOperations.java
@@ -4,8 +4,16 @@ import gr.athenarc.messaging.dto.ThreadDTO;
 
 public interface EmailOperations {
 
+    /**
+     * Sends email messages for the last {@link ThreadDTO#getMessages() message} in the {@link ThreadDTO topic}.
+     * @param threadDTO
+     */
     void sendEmails(ThreadDTO threadDTO);
 
+    /**
+     * Sends reminder messages regarding a {@link ThreadDTO topic}.
+     * @param threadDTO
+     */
     void sendReminder(ThreadDTO threadDTO);
 
     void sendReport(ThreadDTO threadDTO);
--- src/main/java/eu/eosc/observatory/messaging/EmailService.java
+++ src/main/java/eu/eosc/observatory/messaging/EmailService.java
@@ -1,6 +1,7 @@
 package eu.eosc.observatory.messaging;
 
 import eu.eosc.observatory.configuration.ApplicationProperties;
+import eu.eosc.observatory.domain.UserGroup;
 import eu.eosc.observatory.service.GroupService;
 import freemarker.template.Configuration;
 import gr.athenarc.messaging.domain.Correspondent;
@@ -8,18 +9,19 @@ import gr.athenarc.messaging.dto.MessageDTO;
 import gr.athenarc.messaging.dto.ThreadDTO;
 import gr.athenarc.messaging.mailer.client.service.MailClient;
 import gr.athenarc.messaging.mailer.domain.EmailMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.io.StringWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.*;
 
 @Service
 public class EmailService implements EmailOperations {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
     private final MailClient mailClient;
     private final GroupService groupService;
     private final Configuration configuration;
@@ -56,28 +58,15 @@ public class EmailService implements EmailOperations {
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    String createEmailMessageText(ThreadDTO threadDTO, String group, String template) {
-        MessageDTO messageDTO = threadDTO.getMessages().get(threadDTO.getMessages().size() - 1);
-
-        if (messageDTO.isAnonymousSender() && !StringUtils.hasText(messageDTO.getFrom().getGroupId())) {
-            messageDTO.getFrom().setName(groupService.getGroupName(messageDTO.getFrom().getGroupId()));
-        }
-
-        StringWriter stringWriter = new StringWriter();
-        Map<String, String> data = new HashMap<>();
-        data.put("subject", threadDTO.getSubject());
-        data.put("content", messageDTO.getBody());
-        data.put("user", messageDTO.getFrom().getName());
-        data.put("url", String.format("%s/contributions/%s/messages/%s", applicationProperties.getLoginRedirect(), group, threadDTO.getId()));
-        try {
-            configuration.getTemplate(template).process(data, stringWriter);
-        } catch (Exception e) {
-
-        }
-        return stringWriter.getBuffer().toString();
-    }
-
-    List<EmailMessage> createEmails(ThreadDTO thread, String from) {
+    /**
+     * Returns a list of email messages to send when a new message is sent.
+     * It creates a separate {@link EmailMessage email message} for the users of each {@link UserGroup}
+     * based on the {@link UserGroup#id group id}.
+     * @param thread The topic thread.
+     * @param sender The email address to be used as sender.
+     * @return {@link List}<{@link EmailMessage}>
+     */
+    List<EmailMessage> createEmails(ThreadDTO thread, String sender) {
         List<EmailMessage> emails = new ArrayList<>();
 
         MessageDTO messageDTO = thread.getMessages().get(thread.getMessages().size() - 1);
@@ -91,26 +80,71 @@ public class EmailService implements EmailOperations {
 
         for (Map.Entry<String, Set<String>> entry : userGroupEmails.entrySet()) {
             if (StringUtils.hasText(entry.getKey())) {
-                emails.add(createEmail(thread, from, entry.getKey(), entry.getValue(), "message.ftlh"));
+                emails.add(createEmail(thread, sender, entry.getKey(), entry.getValue(), "emails/inlined-css/message.ftlh"));
             } else {
-                emails.add(createEmail(thread, from, entry.getKey(), entry.getValue(), "external-user_message.ftlh"));
+                emails.add(createEmail(thread, sender, entry.getKey(), entry.getValue(), "emails/inlined-css/external-user_message.ftlh"));
             }
         }
 
         return emails;
     }
 
-    EmailMessage createEmail(ThreadDTO thread, String from, String group, Set<String> emails, String template) {
+    /**
+     * Creates an EmailMessage based on the provided info using the {@link EmailMessage.EmailBuilder EmailBuilder}.
+     * It uses {@link EmailService#createEmailBody} method under the hood to generate the email body.
+     * @param thread the thread
+     * @param sender The email address to be used as sender.
+     * @param group The group id of the users to send the message.
+     * @param emails The email addresses of the users to send the message.
+     * @param template The path of the message template file.
+     * @return {@link EmailMessage}
+     */
+    EmailMessage createEmail(ThreadDTO thread, String sender, String group, Set<String> emails, String template) {
         return new EmailMessage.EmailBuilder()
-                .setFrom(from)
+                .setFrom(sender)
 //                .setCc(ccEmails)
                 .setBcc(new ArrayList<>(emails))
                 .setSubject(thread.getSubject())
-                .setText(createEmailMessageText(thread, group, template))
+                .setText(createEmailBody(thread, group, template))
                 .setHtml(true)
                 .build();
     }
 
+    /**
+     * Creates an html email body, based on a <a href="https://freemarker.apache.org">FreeMarker</a> template.
+     * It contains the user, the subject of the {@link ThreadDTO thread}(conversation) and the body of the message sent.
+     * @param threadDTO The {@link ThreadDTO thread}(conversation).
+     * @param group The group id of the users the message is referring.
+     * @param template The path of a <a href="https://freemarker.apache.org">FreeMarker</a> template file.
+     * @return {@link String} The body of the email to send.
+     */
+    String createEmailBody(ThreadDTO threadDTO, String group, String template) {
+        MessageDTO messageDTO = threadDTO.getMessages().get(threadDTO.getMessages().size() - 1);
+
+        if (messageDTO.isAnonymousSender() && StringUtils.hasText(messageDTO.getFrom().getGroupId())) {
+            messageDTO.getFrom().setName(groupService.getGroupName(messageDTO.getFrom().getGroupId()));
+        }
+
+        StringWriter stringWriter = new StringWriter();
+        Map<String, String> data = new HashMap<>();
+        data.put("subject", threadDTO.getSubject());
+        data.put("content", messageDTO.getBody());
+        data.put("user", messageDTO.getFrom().getName());
+        data.put("url", String.format("%s/contributions/%s/messages/%s", applicationProperties.getLoginRedirect(), group, threadDTO.getId()));
+        try {
+            configuration.getTemplate(template).process(data, stringWriter);
+        } catch (Exception e) {
+            logger.warn(e.getMessage(), e);
+        }
+        return stringWriter.getBuffer().toString();
+    }
+
+    /**
+     * Returns a map where each key is a {@link UserGroup#id group id} and its value is the group's users' email.
+     * @param correspondents the list of {@link Correspondent correspondents}
+     * @param isMember whether the returned results will return only member emails or only non-members.
+     * @return {@link Map}<{@link String}, {@link Set}<{@link String}>>
+     */
     Map<String, Set<String>> getUserGroupEmails(List<Correspondent> correspondents, boolean isMember) {
         Map<String, Set<String>> userGroupMap = new TreeMap<>();
         for (Correspondent correspondent : correspondents) {
