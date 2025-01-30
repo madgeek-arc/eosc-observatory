09e510b9edabb9ef44ca89acdd54b9b444630c09
Konstantinos Spyrou <spyroukon@gmail.com>
2025-01-30 14:40:04 +0200

    fix: restore redis configuration

--- src/main/java/eu/eosc/observatory/service/RedisCacheService.java
+++ src/main/java/eu/eosc/observatory/service/RedisCacheService.java
@@ -17,17 +17,11 @@ package eu.eosc.observatory.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
-import jakarta.annotation.PostConstruct;
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 public class RedisCacheService<K, V> implements CacheService<K, V> {
