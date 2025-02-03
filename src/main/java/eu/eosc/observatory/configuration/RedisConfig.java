0d4e4be07fec0716bebc229bb20d113fbd0c7eb8
Konstantinos Spyrou <spyroukon@gmail.com>
2025-02-03 16:36:18 +0200

    fix: set bean primary to avoid autoconfiguration conflicts

--- src/main/java/eu/eosc/observatory/configuration/RedisConfig.java
+++ src/main/java/eu/eosc/observatory/configuration/RedisConfig.java
@@ -17,6 +17,7 @@ package eu.eosc.observatory.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -24,6 +25,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Primary
     @Bean
     <T> RedisTemplate<String, T> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, T> redisTemplate = new RedisTemplate<>();
