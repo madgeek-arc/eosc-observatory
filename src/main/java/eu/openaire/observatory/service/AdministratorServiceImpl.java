af7f30b897cd7b38005207b0663ef8a6a96883f1
Konstantinos Spyrou <23262569+spyroukostas@users.noreply.github.com>
2026-06-10 17:34:33 +0300

    refactor: extract common methods to abstract class to reduce duplication

--- src/main/java/eu/openaire/observatory/service/AdministratorServiceImpl.java
+++ src/main/java/eu/openaire/observatory/service/AdministratorServiceImpl.java
@@ -20,31 +20,19 @@ import eu.openaire.observatory.domain.Administrator;
 import eu.openaire.observatory.domain.SurveyAnswer;
 import eu.openaire.observatory.permissions.Groups;
 import eu.openaire.observatory.permissions.PermissionService;
-import eu.openaire.observatory.permissions.Permissions;
 import gr.uoa.di.madgik.catalogue.service.ModelResponseValidator;
-import gr.uoa.di.madgik.registry.domain.FacetFilter;
 import gr.uoa.di.madgik.registry.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.SortedSet;
-
-import static eu.openaire.observatory.utils.SurveyAnswerUtils.getSurveyAnswerIds;
-
 @Service
-public class AdministratorServiceImpl extends AbstractUserGroupService<Administrator> implements AdministratorService {
+public class AdministratorServiceImpl extends AbstractPermissionedGroupService<Administrator> implements AdministratorService {
 
     private static final Logger logger = LoggerFactory.getLogger(AdministratorServiceImpl.class);
 
     private static final String RESOURCE_TYPE = "administrator";
-
-    private final CrudService<SurveyAnswer> surveyAnswerCrudService;
-    private final PermissionService permissionService;
 
     public AdministratorServiceImpl(ResourceTypeService resourceTypeService,
                                     ResourceService resourceService,
@@ -55,9 +43,7 @@ public class AdministratorServiceImpl extends AbstractUserGroupService<Administr
                                     @Lazy CrudService<SurveyAnswer> surveyAnswerCrudService,
                                     PermissionService permissionService,
                                     ModelResponseValidator validator) {
-        super(userService, resourceTypeService, resourceService, searchService, versionService, parserService, validator);
-        this.permissionService = permissionService;
-        this.surveyAnswerCrudService = surveyAnswerCrudService;
+        super(userService, resourceTypeService, resourceService, searchService, versionService, parserService, surveyAnswerCrudService, permissionService, validator);
     }
 
     @Override
@@ -66,95 +52,14 @@ public class AdministratorServiceImpl extends AbstractUserGroupService<Administr
     }
 
     @Override
+    protected Groups getGroup() {
+        return Groups.ADMINISTRATOR;
+    }
+
+    @Override
     public String createId(Administrator administrator) {
         String id = String.format("admin-%s", administrator.getType());
         logger.debug("Created new ID for Administrator: {}", id);
         return id;
-    }
-
-    @Override
-    public Administrator add(Administrator administrator) {
-        administrator = super.add(administrator);
-        updatePermissions(administrator, Collections.emptySet(), administrator.getMembers());
-        updatePermissions(administrator, Collections.emptySet(), administrator.getAdmins());
-        return administrator;
-    }
-
-    @Override
-    public Administrator update(String id, Administrator administrator) {
-        Administrator existing = get(id);
-        updatePermissions(administrator, existing.getMembers(), administrator.getMembers());
-        updatePermissions(administrator, existing.getAdmins(), administrator.getAdmins());
-        return super.update(id, administrator);
-    }
-
-    @Override
-    public SortedSet<String> updateMembers(String administratorId, Set<String> userIds) {
-        return super.updateMembers(administratorId, userIds, existing -> {
-            updatePermissions((Administrator) existing, existing.getMembers(), userIds);
-        });
-    }
-
-    @Override
-    public SortedSet<String> addMember(String administratorId, String userId) {
-        // read access for all resources
-        permissionService.addPermissions(Collections.singletonList(userId), Collections.singletonList(Permissions.READ.getKey()), getAccessibleResourceIds(administratorId), administratorId);
-        return super.addMember(administratorId, userId);
-    }
-
-    @Override
-    public SortedSet<String> removeMember(String administratorId, String adminId) {
-        // remove Administrator permissions from user
-        permissionService.removeAll(adminId, Groups.ADMINISTRATOR.getKey());
-        permissionService.removeAll(adminId, administratorId);
-        return super.removeMember(administratorId, adminId);
-    }
-
-    @Override
-    public SortedSet<String> updateAdmins(String administratorId, Set<String> adminIds) {
-        return super.updateAdmins(administratorId, adminIds, existing -> {
-            updatePermissions((Administrator) existing, existing.getAdmins(), adminIds);
-        });
-    }
-
-    @Override
-    public SortedSet<String> addAdmin(String administratorId, String adminId) {
-        // read access for all resources
-        permissionService.addPermissions(Collections.singletonList(adminId), Collections.singletonList(Permissions.READ.getKey()), getAccessibleResourceIds(administratorId), administratorId);
-        return super.addAdmin(administratorId, adminId);
-    }
-
-    @Override
-    public SortedSet<String> removeAdmin(String administratorId, String adminId) {
-        // remove Administrator permissions from user
-        permissionService.removeAll(adminId, Groups.ADMINISTRATOR.getKey());
-        permissionService.removeAll(adminId, administratorId);
-        return super.removeAdmin(administratorId, adminId);
-    }
-
-    private void updatePermissions(Administrator administrator, Set<String> existingUsers, Set<String> userIds) {
-        for (String member : userIds) {
-            existingUsers.remove(member);
-        }
-
-        // remove Administrator permissions from removed members
-        permissionService.removeAll(existingUsers, Groups.ADMINISTRATOR.getKey());
-        permissionService.removeAll(existingUsers, administrator.getId());
-
-        // read access for all resources
-        permissionService.addPermissions(userIds, Collections.singletonList(Permissions.READ.getKey()), getAccessibleResourceIds(administrator), administrator.getId());
-    }
-
-    private List<String> getAccessibleResourceIds(Administrator administrator) {
-        // read access for all resources
-        FacetFilter filter = new FacetFilter();
-        filter.setQuantity(10000);
-        filter.addFilter("type", administrator.getType());
-        List<SurveyAnswer> resourceIds = surveyAnswerCrudService.getAll(filter).getResults();
-        return getSurveyAnswerIds(resourceIds);
-    }
-
-    private List<String> getAccessibleResourceIds(String administratorId) {
-        return getAccessibleResourceIds(get(administratorId));
     }
 }
--- src/main/java/eu/openaire/observatory/service/CoordinatorServiceImpl.java
+++ src/main/java/eu/openaire/observatory/service/CoordinatorServiceImpl.java
@@ -20,31 +20,19 @@ import eu.openaire.observatory.domain.Coordinator;
 import eu.openaire.observatory.domain.SurveyAnswer;
 import eu.openaire.observatory.permissions.Groups;
 import eu.openaire.observatory.permissions.PermissionService;
-import eu.openaire.observatory.permissions.Permissions;
 import gr.uoa.di.madgik.catalogue.service.ModelResponseValidator;
-import gr.uoa.di.madgik.registry.domain.FacetFilter;
 import gr.uoa.di.madgik.registry.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.SortedSet;
-
-import static eu.openaire.observatory.utils.SurveyAnswerUtils.getSurveyAnswerIds;
-
 @Service
-public class CoordinatorServiceImpl extends AbstractUserGroupService<Coordinator> implements CoordinatorService {
+public class CoordinatorServiceImpl extends AbstractPermissionedGroupService<Coordinator> implements CoordinatorService {
 
     private static final Logger logger = LoggerFactory.getLogger(CoordinatorServiceImpl.class);
 
     private static final String RESOURCE_TYPE = "coordinator";
-
-    private final CrudService<SurveyAnswer> surveyAnswerCrudService;
-    private final PermissionService permissionService;
 
     public CoordinatorServiceImpl(ResourceTypeService resourceTypeService,
                                   ResourceService resourceService,
@@ -55,9 +43,7 @@ public class CoordinatorServiceImpl extends AbstractUserGroupService<Coordinator
                                   @Lazy CrudService<SurveyAnswer> surveyAnswerCrudService,
                                   PermissionService permissionService,
                                   ModelResponseValidator validator) {
-        super(userService, resourceTypeService, resourceService, searchService, versionService, parserService, validator);
-        this.permissionService = permissionService;
-        this.surveyAnswerCrudService = surveyAnswerCrudService;
+        super(userService, resourceTypeService, resourceService, searchService, versionService, parserService, surveyAnswerCrudService, permissionService, validator);
     }
 
     @Override
@@ -66,95 +52,14 @@ public class CoordinatorServiceImpl extends AbstractUserGroupService<Coordinator
     }
 
     @Override
+    protected Groups getGroup() {
+        return Groups.COORDINATOR;
+    }
+
+    @Override
     public String createId(Coordinator coordinator) {
         String id = String.format("co-%s", coordinator.getType());
         logger.debug("Created new ID for Coordinator: {}", id);
         return id;
-    }
-
-    @Override
-    public Coordinator add(Coordinator coordinator) {
-        coordinator = super.add(coordinator);
-        updatePermissions(coordinator, Collections.emptySet(), coordinator.getMembers());
-        updatePermissions(coordinator, Collections.emptySet(), coordinator.getAdmins());
-        return coordinator;
-    }
-
-    @Override
-    public Coordinator update(String id, Coordinator coordinator) {
-        Coordinator existing = get(id);
-        updatePermissions(coordinator, existing.getMembers(), coordinator.getMembers());
-        updatePermissions(coordinator, existing.getAdmins(), coordinator.getAdmins());
-        return super.update(id, coordinator);
-    }
-
-    @Override
-    public SortedSet<String> updateMembers(String coordinatorId, Set<String> userIds) {
-        return super.updateMembers(coordinatorId, userIds, existing -> {
-            updatePermissions((Coordinator) existing, existing.getMembers(), userIds);
-        });
-    }
-
-    @Override
-    public SortedSet<String> addMember(String coordinatorId, String userId) {
-        // read access for all resources
-        permissionService.addPermissions(Collections.singletonList(userId), Collections.singletonList(Permissions.READ.getKey()), getAccessibleResourceIds(coordinatorId), coordinatorId);
-        return super.addMember(coordinatorId, userId);
-    }
-
-    @Override
-    public SortedSet<String> removeMember(String coordinatorId, String adminId) {
-        // remove Coordinator permissions from user
-        permissionService.removeAll(adminId, Groups.COORDINATOR.getKey());
-        permissionService.removeAll(adminId, coordinatorId);
-        return super.removeMember(coordinatorId, adminId);
-    }
-
-    @Override
-    public SortedSet<String> updateAdmins(String coordinatorId, Set<String> adminIds) {
-        return super.updateAdmins(coordinatorId, adminIds, existing -> {
-            updatePermissions((Coordinator) existing, existing.getAdmins(), adminIds);
-        });
-    }
-
-    @Override
-    public SortedSet<String> addAdmin(String coordinatorId, String adminId) {
-        // read access for all resources
-        permissionService.addPermissions(Collections.singletonList(adminId), Collections.singletonList(Permissions.READ.getKey()), getAccessibleResourceIds(coordinatorId), coordinatorId);
-        return super.addAdmin(coordinatorId, adminId);
-    }
-
-    @Override
-    public SortedSet<String> removeAdmin(String coordinatorId, String adminId) {
-        // remove Coordinator permissions from user
-        permissionService.removeAll(adminId, Groups.COORDINATOR.getKey());
-        permissionService.removeAll(adminId, coordinatorId);
-        return super.removeAdmin(coordinatorId, adminId);
-    }
-
-    private void updatePermissions(Coordinator coordinator, Set<String> existingUsers, Set<String> userIds) {
-        for (String member : userIds) {
-            existingUsers.remove(member);
-        }
-
-        // remove Coordinator permissions from removed members
-        permissionService.removeAll(existingUsers, Groups.COORDINATOR.getKey());
-        permissionService.removeAll(existingUsers, coordinator.getId());
-
-        // read access for all resources
-        permissionService.addPermissions(userIds, Collections.singletonList(Permissions.READ.getKey()), getAccessibleResourceIds(coordinator), coordinator.getId());
-    }
-
-    private List<String> getAccessibleResourceIds(Coordinator coordinator) {
-        // read access for all resources
-        FacetFilter filter = new FacetFilter();
-        filter.setQuantity(10000);
-        filter.addFilter("type", coordinator.getType());
-        List<SurveyAnswer> resourceIds = surveyAnswerCrudService.getAll(filter).getResults();
-        return getSurveyAnswerIds(resourceIds);
-    }
-
-    private List<String> getAccessibleResourceIds(String coordinatorId) {
-        return getAccessibleResourceIds(get(coordinatorId));
     }
 }
